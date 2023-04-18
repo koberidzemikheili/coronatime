10e6f03208825134a447807a2548992668be4ad5
koberidzemikheili <mikheil.koberidze@redberry.ge>
2023-04-18 11:46:37 +0400

    feat: add language middleware

--- resources/views/components/lang-button.blade.php
+++ resources/views/components/lang-button.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ open: false, selected: 'en' }">
+<div x-data="{ open: false, selected: '{{ app()->getLocale() }}' }">
     <div>
         <button x-on:click="open = !open" class="inline-flex justify-center w-full">
             <span x-text="selected === 'en' ? 'English' : 'ქართული'"></span>
@@ -10,9 +10,9 @@
         <div x-show="open" x-on:click.away="open = false"
          class="origin-top-right absolute mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
             <div class="py-1">
-                <a x-on:click.prevent="selected = 'en'; open = false;" href="#" 
+                <a x-on:click.prevent="selected = 'en'; open = false; window.location.href = '?lang=en';" 
                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">English</a>
-                <a x-on:click.prevent="selected = 'ka'; open = false;" href="#" 
+                <a x-on:click.prevent="selected = 'ka'; open = false; window.location.href = '?lang=ka';"
                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">ქართული</a>
             </div>
         </div>
